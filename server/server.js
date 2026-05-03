2739034ec03eafffc05b7574c04e3a0a83157d9c
Misge <misgechekole06@gmail.com>
2026-05-03 18:31:26 +0300

    API Created for Dashboard data

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import employeesRouter from "./routes/employeeRoutes.js";
 import profileRouter from "./routes/profileRoutes.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
 import leaveRouter from "./routes/leaveRoutes.js";
+import dashboardRouter from "./routes/dashboardRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -29,6 +30,7 @@ app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
 app.use("/api/leaves", leaveRouter);
 app.use("/api/payslips", payslipRouter);
+app.use("/api/dashboard", dashboardRouter);
 
 const startServer = async () => {
   try {
